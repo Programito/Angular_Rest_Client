793e413ebe24c716c2306ac9dbce4aced3966f28
Programito <Programito@gmail.com>
2019-01-02 18:21:58 +0100

    first commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Juan', apellido: 'Gil', email: 'Juan_Gil@gmail.com', createAt:'2015-10-11'},
+  {id: 2, nombre: 'Alan', apellido: 'Sheare', email: 'Alan_Sheare@gmail.com', createAt:'2015-11-15'},
+  {id: 3, nombre: 'David', apellido: 'Perez', email: 'David_Perez@gmail.com', createAt:'2017-10-16'},
+  {id: 4, nombre: 'Helena', apellido: 'Lopez', email: 'Helena_Perez@gmail.com', createAt:'2017-12-10'},
+  {id: 5, nombre: 'Jessica', apellido: 'Gata', email: 'Jessica_Gata@gmail.com', createAt:'2018-11-16'},
+  {id: 6, nombre: 'Michael', apellido: 'Kloss', email: 'Michael_Kloss@gmail.com', createAt:'2017-10-11'},
+  {id: 7, nombre: 'Pablo', apellido: 'Contreras', email: 'Pablo_Contreras@gmail.com', createAt:'2017-10-18'},
+  {id: 8, nombre: 'Cristina', apellido: 'Santos', email: 'Cristina_Santos@gmail.com', createAt:'2017-10-20'},
+  {id: 9, nombre: 'Marta', apellido: 'Guerrero', email: 'Marta_G@gmail.com', createAt:'2017-10-21'},
+  {id: 10, nombre: 'Eloy', apellido: 'Pascual', email: 'Eloy_Pascual@gmail.com', createAt:'2017-10-22'},
+  {id: 11, nombre: 'Elvira', apellido: 'Rodriguez', email: 'Elvira_Rodriguez@gmail.com', createAt:'2017-10-17'},
+];
